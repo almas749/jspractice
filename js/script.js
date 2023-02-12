017baa494673cc766bad665f6d38d92f15ccb628
almas <shegetnpanda@gmail.com>
2023-02-12 10:35:29 +0600

    practical task 2

--- js/script.js
+++ js/script.js
@@ -1,216 +1,76 @@
 'use strict';
 
-// *
-// **
-// ***
-// ****
-// *****
-// ******
-// let star = '';
+/* Задание на урок:
 
-// for (let i = 1; i < 7; i++) {
-//     console.log(star);
-//     for (let j = 1; j < 7; j++) {
-//         star +='*';
-//         break;
-//     }
-// }
-
-// for (let i = 1; i < 7; i++) {
-//     for (let j = 0; j < i; j++) {
-//         star +='*';
-//     }
-//     star += '\n';
-// }
-
-// console.log(star);
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
 
-//first - метка
-// first: for (let i = 0; i < 3; i++) {
-//     console.log(`First level: ${i}`);
-//     for (let j = 0; j < 3; j++) {
-//         console.log(`Second level: ${j}`);
-//         for (let k = 0; k < 3; k++) {
-//             if (k == 2) continue first;
-//             console.log(`Third level: ${k}`);
-//         }
-//     }
-// }
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
 
-// 1 При помощи цикла выведите числа от 5 до 10 в консоль. 5 и 10 включительно. Цикл можно использовать любой 
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+// Код возьмите из предыдущего домашнего задания
 
 
-// 2 При помощи цикла for вывести числа от 20 до 10 в консоль. В обратном порядке (20, 19, 18...). Когда цикл дойдет до числа 13 - остановить весь цикл
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', ''); 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+
+    },
+    actors: {
+
+    },
+    genres: [
+
+    ],
+    privat: false
+};
 
 
-// 3 При помощи цикла for выведите чётные числа от 2 до 10 включительно
+for (let i = 0; i < 2; i++) {
+    const watchedMovie = prompt('Один из последних просмотренных фильмов?', ''),
+          rateMovie = prompt('На сколько оцените его?', '');
 
-
-// 4 Перепишите цикл  for на вариант с while. Результат должен остаться точно таким же. Не создайте бесконечный цикл! Иначе браузер может зависнуть.
-//  Цикл, который нужно переписать:
- 
-//  for (let i = 2; i <= 16; i++) {
-//      if (i % 2 === 0) {
-//          continue;
-//      } else {
-//          console.log(i);
-//      }
-//  }
-
-
-// 5 Заполните массив цифрами от 5 до 10 включительно. Помните, что элементы массива можно сформировать так же, как и обращаться к ним: arr[0]
-
-// Место для первой задачи
-function firstTask() {
-    // Пишем решение вот тут
-    for (let i = 5; i <=10; i++) {
-        console.log(i);
+    if(watchedMovie != null && rateMovie != null && watchedMovie != '' && rateMovie != '' && watchedMovie.length < 50){
+        personalMovieDB.movies[watchedMovie] = rateMovie;
+        console.log('done!');
+    } else {
+        console.log('error!');
+        i--;
     }
-    
+}
+console.log(personalMovieDB);
+
+let i = 0;
+while (i < 2) {
+    const watchedMovie = prompt('Один из последних просмотренных фильмов?', ''),
+          rateMovie = prompt('На сколько оцените его?', '');
+
+    if(watchedMovie != null && rateMovie != null && watchedMovie != '' && rateMovie != '' && watchedMovie.length < 50){
+        personalMovieDB.movies[watchedMovie] = rateMovie;
+        console.log('done!');
+    } else {
+        console.log('error!');
+        i--;
+    }
+    i++;
 }
 
-// Место для второй задачи
-function secondTask() {
-    // Пишем решение вот тут
-    for (let i = 20; i >=10; i--) {
-        if(i === 13) break;
-        console.log(i);
-    }
-    
+console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
 }
-
-// Место для третьей задачи
-function thirdTask() {
-    // Пишем решение вот тут
-    for (let i = 2; i <= 10; i++) {
-        if(i % 2 === 0) console.log(i);
-    }
-    
-}
-
-// Место для четвертой задачи
-
-// Цикл, который нужно переписать:
-
-// for (let i = 2; i <= 16; i++) {
-//     if (i % 2 === 0) {
-//         continue;
-//     } else {
-//         console.log(i);
-//     }
-// }
-
-function fourthTask() {
-    // Пишем решение вот тут
-    let i = 1;
-    while (i < 16) {
-        i++;
-        if (i % 2 === 0) {
-            continue;
-        }
-        console.log(i);
-    }
-    
-}
-
-// Место для пятой задачи
-
-function fifthTask() {
-    const arrayOfNumbers = [];
-
-    // Пишем решение вот тут
-    for (let i = 0; i < 6; i++) {
-        arrayOfNumbers[i] = i + 5;
-    }
-    
-    
-    // Не трогаем
-    return arrayOfNumbers;
-}
-
-// Задачи:
-
-// 1 Заполните новый массив (result) числами из старого (arr). Количество элементов в массиве можно получить как arr.length, а к элементам обращаемся все так же: arr[0], arr[1] и тд.
-// Должен получиться точно такой же массив
-
-// 2 Измените данный массив так, чтобы все числа были увеличены в 2 раза, а если попадается строка строка - то к ней было добавлено " - done".
-// Для определения типа данных используйте typeof();
-// Должно получиться: [ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
-
-// 3 Разверните массив data наоборот при помощи цикла и запишите данные в массив result.
-// Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
-
-// Место для первой задачи
-function firstTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const arr = [3, 5, 8, 16, 20, 23, 50];
-    const result = [];
-
-    // Пишем решение вот тут
-    for (let i = 0; i < arr.length; i++) {
-        result[i] = arr[i];
-    }
-
-    console.log(result);
-    
-    // Не трогаем
-    return result;
-}
-
-// Место для второй задачи
-function secondTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-
-    // Пишем решение вот тут
-    for (let i = 0; i < data.length; i++) {
-        if (typeof(data[i]) === 'string') {
-            data[i] += ' - done';
-        } else {
-            data[i] *= 2;
-        }
-    }
-    // Не трогаем
-    return data;
-}
-
-// Место для третьей задачи
-function thirdTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-    const result = [];
-
-        for (let i = 0; i < data.length; i++) {
-            result[i] = data[(data.length - 1) - i];
-        }
-
-    console.log(result);
-    // Пишем решение вот тут
-}
-
-// Иногда можно встретить просто нереальные вещи, построенные только при помощи кода. Но мы с вами займемся базовой фигурой. Сейчас вам необходимо написать код, который при помощи звездочек (*) в консоли нарисует вот такую фигуру:
-
-//      *
-//     ***
-//    *****
-//   *******
-//  *********
-// ***********
-// (Подсказка: в конце фигуры есть перенос строки \n, который тоже учитывается в тестах. В КОНЦЕ КАЖДОЙ СТРОКИ НЕТ ПРОБЕЛОВ, ТОЛЬКО ПЕРЕНОС)
-
-
-const lines = 5;
-let result = '';
-for (let i = 0; i <= lines; i++) {
-    for (let j = 0; j < lines - i; j++) {
-        result += ' ';
-    }
-    for (let j = 0; j < 2 * i + 1; j++) {
-        result += "*"; 
-    }
-    result += '\n';
-}
-
-console.log(result);
-
