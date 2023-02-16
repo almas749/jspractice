817b40d5422cdc52f7572b60642d8357c7bda2c4
almas <shegetnpanda@gmail.com>
2023-02-16 09:50:46 +0600

    arrays

--- js/script.js
+++ js/script.js
@@ -1,37 +1,32 @@
 'use strict';
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        background: 'red'
-    },
-    makeTest: function() {
-        console.log('test');
-    }
-};
-
-options.makeTest();
-
-const {border, bg} = options.colors;
-
-console.log(border);
-
-console.log(Object.keys(options).length);
-
-// console.log(options.name);
-let counter = 0;
-for (let key in options) {
-    if (typeof(options[key]) === 'object') {
-        for(let i in options[key]){
-            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-            counter++;
-        }
-    } else {
-        console.log(`Свойство ${key} имеет значение ${options[key]}`);
-        counter++;
-    }
+const arr = [154, 22, 368, 66, 848];
+arr.sort(compareNum);
+console.log(arr);
+function compareNum (a, b) {
+    return a - b;
 }
-console.log(`Количество свойств: ${counter}`);
+
+console.log(arr.length);
+
+arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item} внутри массива ${arr}`);
+});
+
+arr.pop();
+arr.push(10);
+
+console.log(arr);
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+for (let value of arr) {
+    console.log(value); 
+}   
+
+const str = prompt("", "");
+const products = str.split(", ");
+products.sort();
+console.log(products.join('; ')); 
