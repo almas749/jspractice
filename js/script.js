284ae3f5e1a2876c71bd823513b1c577fda07fef
almas <shegetnpanda@gmail.com>
2023-02-17 13:31:37 +0600

    thirteenth task

--- js/script.js
+++ js/script.js
@@ -1,88 +1,62 @@
 'use strict';
 
-/* Задание на урок:
+// Задача:
 
-1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
-перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
-Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+// У вас есть небольшой кусочек данных о торговом центре, которые записаны в объекте shoppingMallData. Они содержат массив с данными о магазинах, где указана длина и ширина помещения; высоту помещения; стоимость отопления за 1 кубический метр и бюджет на оплату отопления за месяц.
 
-2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
-переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+// Основная задача - это написать функцию isBudgetEnough, которая буде возвращать строку. Если бюджета хватает для отопления всего объема торгового центра - выводится 'Бюджета достаточно', если нет - 'Бюджета недостаточно'. И все 
 
-3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
-Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
-при помощи метода forEach вывести в консоль сообщения в таком виде:
-"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+// Но эта задача содержит несколько подзадач внутри:
 
+// - вычисление общей площади всех магазинов, которая вычисляется как длина магазина, умноженная на его ширину;
 
-const personalMovieDB = {
-    count: 0,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-    start: function () {
-        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// - вычисление общего объема торгового центра, так как цена отопления указана в кубических метрах;
+
+// - определение того, хватает ли бюджета на оплату такого объема;
+
+// - все числа идут без единиц измерения для упрощения, просто цифры и все;
+
+// - функция должна продолжать работать, даже если изменяется количество магазинов, высота, бюджет или подставляется вообще другой объект.
+
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
         }
-    },
-    rememberMyFilms: function() {
-        for (let i = 0; i < 2; i++) {
-            const watchedMovie = prompt('Один из последних просмотренных фильмов?', ''),
-                  rateMovie = prompt('На сколько оцените его?', '');
-        
-            if(watchedMovie != null && rateMovie != null && watchedMovie != '' && rateMovie != '' && watchedMovie.length < 50){
-                personalMovieDB.movies[watchedMovie] = rateMovie;
-                console.log('done!');
-            } else {
-                console.log('error!');
-                i--;
-            }
-        }
-    },
-    detectPersonalLevel: function () {
-        if (personalMovieDB.count < 10) {
-            console.log('Просмотрено довольно мало фильмов');
-        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-            console.log('Вы классический зритель');
-        } else if (personalMovieDB.count >= 30) {
-            console.log('Вы киноман');
-        } else {
-            console.log('Произошла ошибка');
-        }
-    },
-    showMyDB: function () {
-        if (!personalMovieDB.privat) {
-            console.log(personalMovieDB);
-        } 
-    },
-    writeYourGenres: function () {
-        for (let i = 0; i < 3; i++) {     
-            let favoriteGenre = prompt(`Ваш любимый жанр под номером ${i + 1}`, '');
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+}
 
-            if (favoriteGenre === '' || favoriteGenre == null) {
-                console.log('Вы не ввели данные');
-                i--;
-            } else {
-                personalMovieDB.genres[i] = favoriteGenre;
-            }
-        }
-        personalMovieDB.genres.forEach((item, i) => {
-            console.log(`Любимый жанр ${i + 1} - это ${item}`);
-        });
-    },
-    toggleVisibleMyDB: function() {
-        if (personalMovieDB.privat) {
-            personalMovieDB.privat = false;
-        } else {
-            personalMovieDB.privat = true;
-        }
+function isBudgetEnough (mall) {
+    let allArea = 0;
+    let message = '';
+    mall.shops.forEach(shop => {
+        let eachArea = shop.width * shop.length;
+        allArea += eachArea;
+    });
+    let allVolume = allArea * mall.height;
+    let cost = allVolume * mall.moneyPer1m3;
+    if ((mall.budget - cost) < 0) {
+        message = `Бюджета недостаточно ${cost}`;
+    } else {
+        message = `Бюджета достаточно ${cost}`;
     }
-};
+    return message;
+}
 
-
-
-
-
-
+console.log(isBudgetEnough(shoppingMallData));
