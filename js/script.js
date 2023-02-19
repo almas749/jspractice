f1f5a7fc87300e5cfbec4a96b7477e4f87038327
almas <shegetnpanda@gmail.com>
2023-02-19 12:48:44 +0600

    get elements from page

--- js/script.js
+++ js/script.js
@@ -1,70 +1,22 @@
 'use strict';
 
-// Задание:
+const box = document.getElementById('box');
 
-// У вас есть объект с данными о ресторане. Начинающий разработчик создал несколько функций, которые работают неправильно и он не может понять почему. Нужно исправить функции так, чтобы они давали всегда правильный результат.
+console.log(box);
 
-// 1) Функция isOpen не хочет правильно работать. Что мы уже не пробовали подставлять в неё - результат все время неправильный. Необходимо найти причины и исправить.
+const btns = document.getElementsByTagName('button');
 
-// 2) Функция isAverageLunchPriceTrue должна брать цены двух любых блюд из меню, складывать их и сравнивать с средним чеком (averageLunchPrice).
+console.log(btns[1]);
 
-// Сейчас функция работает, но постоянно выдает неправильный результат. Ведь из представленного меню сумма двух любых цен всегда будет больше 20. Необходимо найти причину и исправить.
+const circles = document.getElementsByClassName('circle');
 
-// 3) Функция transferWaitors создана для того, чтобы копировать шаблон данных и передавать их в другой ресторан. Конечно, в другом ресторане будут другие блюда, другие официанты и тп. Сейчас эта функция только в начале разработки и должна менять данные про официантов.
+console.log(circles);
 
-// Но в нынешнем виде мы обнаружили, что после её запуска не только копия данных содержит новых официантов, но и основные данные! В restorantData сотрудник Alice исчезает и заменяется Mike! Необходимо найти причину и немедленно исправить, чтобы данные были разделены.
+const hearts = document.querySelectorAll('.heart');
 
-const restorantData = {
-    menu: [
-        {
-            name: 'Salad Caesar',
-            price: '14$'
-        },
-        {
-            name: 'Pizza Diavola',
-            price: '9$'
-        },
-        {
-            name: 'Beefsteak',
-            price: '17$'
-        },
-        {
-            name: 'Napoleon',
-            price: '7$'
-        }
-    ],
-    waitors: [
-        {name: 'Alice', age: 22}, {name: 'John', age: 24}
-    ],
-    averageLunchPrice: '20$',
-    openNow: true
-};
+hearts.forEach(item => {
+    console.log(item);
+})
 
-
-function isOpen(prop) {
-    let answer = '';
-    prop ? answer = 'Открыто' : answer = 'Закрыто';
-
-    return answer;
-}
-
-console.log(isOpen(restorantData.openNow))
-
-function isAverageLunchPriceTrue(fDish, sDish, average) {
-    if ((+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1))) < +average.slice(0, -1)) {
-        return 'Цена ниже средней';
-    } else {
-        return 'Цена выше средней';
-    }
-}
-
-console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
-
-function transferWaitors(data) {
-    const copy = Object.assign({}, data);
-
-    copy.waitors = [{name: 'Mike', age: 32}];
-    return copy;
-}
-
-transferWaitors(restorantData);
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
