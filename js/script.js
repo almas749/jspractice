089892c57a074795e0235d7d6b087fb1a362b395
almas <shegetnpanda@gmail.com>
2023-03-03 14:45:00 +0600

    array methods

--- js/script.js
+++ js/script.js
@@ -1,51 +1,63 @@
 'use strict';
 
-console.log('Запрос данных...');
+// filter
 
-const req = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log('Подготовка данных...');
+// const names = ['Alesha', 'Biba', 'Maksat', 'Shynauyk', 'Ibrahimovic'];
 
-        const product = {
-            name: 'TV',
-            price: 2000
-        };
-
-        resolve(product);
-    }, 2000);
-});
-
-req.then((product) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            product.status = 'order';
-            resolve(product);
-        }, 2000);
-    });
-}).then(data => {
-    data.modify = true;
-    return data;
-}).then(data => {
-    console.log(data);
-}).catch(() => {
-    console.error('Произошла ошибка');
-}).finally(() => {
-    console.log('Finally');
-});
-
-const test = time => {
-    return new Promise(resolve => {
-        setTimeout(() => resolve(), time);
-    });
-};
-
-// test(1000).then(() => console.log('1000 ms'));
-// test(2000).then(() => console.log('2000 ms'));
-
-// Promise.all([test(1000), test(2000)]).then(() => {
-//     console.log('all');
+// const shortNames = names.filter((name) => {
+//     return name.length <= 6;
 // });
 
-Promise.race([test(1000), test(2000)]).then(() => {
-    console.log('first');
-});
+// console.log(shortNames);
+
+
+// map
+
+// const answers = ['TOnY', 'GRiSHA', 'SalaM'];
+
+// const result = answers.map(item => item.toLowerCase());
+
+// console.log(result);
+
+
+// every/some
+
+// const samu = [4, 'awaw', 'ooff'];
+
+// console.log(samu.some(item => typeof(item) === 'number'));
+
+// console.log(samu.every(item => typeof(item) === 'number'));
+
+
+// reduce
+
+// const arr = [4, 5, 1, 3, 6, 2];
+
+//                         // 0      4
+//                         // 4      5
+//                         // 9      1
+//                         // 10     3
+// const res = arr.reduce((sum, current) => sum + current);
+// console.log(res);
+
+const arr = ['almurt', 'alma', 'banan'];
+
+                        // 0      4
+                        // 4      5
+                        // 9      1
+                        // 10     3
+const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+console.log(res);
+
+const obj = {
+    ivan: 'person',
+    ann: 'person',
+    dog: 'animal',
+    cat: 'animal'
+};
+
+const newArr = Object.entries(obj) //превращает объект в массив массивов
+.filter(item => item[1] === 'person')
+.map(item => item[0]); 
+
+console.log(newArr);
