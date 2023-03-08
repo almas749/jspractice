86b2c71a4c4424aaa1dd66b197505bb4acca413f
almas <shegetnpanda@gmail.com>
2023-03-08 14:06:13 +0600

    regular expressions

--- js/script.js
+++ js/script.js
@@ -1,40 +1,35 @@
 'use strict';
 
-// Задачи:
+// new RegExp('pattern', 'flags');
+// /pattern/f
 
-// 1) У вас есть небольшой массив с данными о доходах каждой торговой точки. Напишите функцию getPositiveIncomeAmount, которая принимает этот массив данных и возвращает сумму только положительных значений из каждого объекта. (число)
+// const ans = prompt('Введите ваше число');
 
-// Пример:
+// const reg = /\d/ig;
+// console.log(ans.match(reg));
 
-// getPositiveIncomeAmount(funds) => 13300
+const str = 'My name is R2D2';
 
-// 2) Напишите функцию getTotalIncomeAmount, которая тоже принимает этот массив данных. Если хотя бы один из объектов содержит отрицательное значение поля amount, то функция возвращает сумму всех значений. (число) Если таких значений нет - запускается функция getPositiveIncomeAmount с тем же массивом данных.
+console.log(str.match(/\w\d\w\d/i));
 
-// Пример:
+// \D not digits
+// \W not words
 
-// getTotalIncomeAmount(funds) => -500
+// \d digits
+// \w words
+// \s spaces
 
-const funds = [
-    {amount: -1400},
-    {amount: 2400},
-    {amount: -1000},
-    {amount: 500},
-    {amount: 10400},
-    {amount: -11400}
-];
+// i найти вне зависимости регистра
+// g найти несколько вхождений
+// m включает многострочный режим
 
-const getPositiveIncomeAmount = (data) => {
-    return data.filter(item => (item['amount'] >= 0))
-    .map(item => item['amount'])
-    .reduce((sum, current) => sum + current);
-};
+// console.log(ans.search(reg));
+// console.log(ans.match(reg));
 
-const getTotalIncomeAmount = (data) => {
-    if (data.some(item => (item['amount'] < 0))) {
-        return data.map(item => item['amount'])
-        .reduce((sum, current) => sum + current);
-    } else {
-        getPositiveIncomeAmount(data);
-    }
-};
+// const pass = prompt('Password');
+
+// console.log(pass.replace(/./g, '*'));
+
+// console.log('12-34-56'.replace(/-/g, ':'));
+
 
