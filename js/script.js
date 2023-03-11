27a467b9070b31e1ca1c775052e207243b8cc7fe
almas <shegetnpanda@gmail.com>
2023-03-11 13:49:37 +0600

    incapsulation

--- js/script.js
+++ js/script.js
@@ -1,19 +1,50 @@
 'use strict';
 
-const person = {
-    name: 'Alex',
-    age: 25,
+class User {
 
-    get PersonAge() {
-        return this.age;
-    },
-
-    set PersonAge(num) {
-        this.age = num;
+    constructor(name, age) {
+        this.name = name;
+        this._age = age;
     }
-};
 
-console.log(person.PersonAge = 30);
-console.log(person.PersonAge);
+    #status = 'jobless';
+
+    say() {
+        console.log(`Имя пользователя: ${this.name}, Возраст: ${this._age}`);
+    }
+
+    get status() {
+        return this.#status;
+    }
+
+    set status(st) {
+        if(typeof(st) === 'string') {
+            this.#status = st;
+        } else {
+            console.log('Недопустимое значение');
+        }
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(age) {
+        if(typeof(age) === 'number' && age > 0 && age < 110) {
+            this._age = age;
+        } else {
+            console.log('Недопустимое значение');
+        }
+    }
+}
+
+const alesha = new User('Alesha', 21);
+console.log(alesha.age);   
+alesha.age = 99;
+console.log(alesha.age);   
+alesha.status = 'odinok';
+console.log(alesha.status);
+
+alesha.say();
 
 
