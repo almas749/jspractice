482e3f946af2e109c44a1c9cdba4211226752e16
almas <shegetnpanda@gmail.com>
2023-03-11 14:05:48 +0600

    module

--- js/script.js
+++ js/script.js
@@ -1,50 +1,15 @@
 'use strict';
 
-class User {
+const user = (function() {
+    const privat = function() {
+        console.log('i am private');
+    };
 
-    constructor(name, age) {
-        this.name = name;
-        this._age = age;
-    }
+    return {
+        say: privat
+    };
+}());
 
-    #status = 'jobless';
-
-    say() {
-        console.log(`Имя пользователя: ${this.name}, Возраст: ${this._age}`);
-    }
-
-    get status() {
-        return this.#status;
-    }
-
-    set status(st) {
-        if(typeof(st) === 'string') {
-            this.#status = st;
-        } else {
-            console.log('Недопустимое значение');
-        }
-    }
-
-    get age() {
-        return this._age;
-    }
-
-    set age(age) {
-        if(typeof(age) === 'number' && age > 0 && age < 110) {
-            this._age = age;
-        } else {
-            console.log('Недопустимое значение');
-        }
-    }
-}
-
-const alesha = new User('Alesha', 21);
-console.log(alesha.age);   
-alesha.age = 99;
-console.log(alesha.age);   
-alesha.status = 'odinok';
-console.log(alesha.status);
-
-alesha.say();
+user.say();
 
 
