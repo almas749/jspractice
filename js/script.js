0516f936a96123d02598cd3d5b7db18bcba9b557
almas <shegetnpanda@gmail.com>
2023-03-16 14:29:04 +0600

    animate

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
 'use strict';
 
-const box = document.querySelector('.box');
+const box = document.querySelector('.box'),
+      circles = document.querySelectorAll('.circle');
 let pos = 0;
+
+let circlesAnimation;
+circles[0].addEventListener('click', () => {
+    if(!circlesAnimation) {
+        circlesAnimation = circles[0].animate([
+            {transform: 'translateX(0)',
+            filter: 'opacity(100%)'},
+            {transform: 'translateX(200px)',
+            filter: 'opacity(50%)'},
+            {transform: 'translateX(100px)',
+            filter: 'opacity(75%)'},
+            {transform: 'translateX(0)',
+            filter: 'opacity(100%)'}
+        ], {
+            duration: 3000,
+            iterations: Infinity 
+        });
+    } else if (circlesAnimation.playState === 'paused') {
+        circlesAnimation.play();
+    } else {
+        circlesAnimation.pause();
+    }
+}) 
 
 function myAnimation() {
     pos++;
